bf9fe5557f5a15e94399ba940ba610788add4e00
Maxbo <maxbo2000@web.de>
2024-09-10 12:20:38 +0200

    SQLite DB EntityFramework class

---
+++ Backend/DatabaseContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata;
+
+namespace Backend
+{
+    public class DatabaaseContext : DbContext
+    {
+        //DBSets
+
+
+        public string DbPath { get; }
+
+        public DatabaaseContext()
+        {
+            var path = Directory.GetCurrentDirectory();
+            DbPath = System.IO.Path.Join(path, "gqm.db");
+        }
+
+        // The following configures EF to create a Sqlite database file in the
+        // current folder
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+    }
+}
